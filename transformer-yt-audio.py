9f94502141c780408039d2be116e976b4a9839e6
DanRod1 <drodrigu1971@gmail.com>
2023-10-24 13:59:32 +0200

    Srt subtitles files fonction

--- transformer-yt-audio.py
+++ transformer-yt-audio.py
@@ -14,6 +14,40 @@ import whisper
 import proglog
 import traceback
 import argparse
+import re
+from datetime import timedelta
+
+def generateSrt(srtFile: any , data :dict = {}, outputLanguage :str = '', init: list = ['00','00','00,000']):
+    for keys in data :
+        pattern = '\d+'
+        result = re.search(pattern=pattern,string=keys) 
+        index = int(result.group())
+        seconds = (index + 1) * 10
+        end = str(timedelta(seconds=seconds)).split(':')
+        print(f'{index}')
+        srtFile.write(f'{index}\n')
+        print(f'{init}')
+        srtFile.write(f'{":".join(init)} --> ')
+        count = 0
+        returnValue = []
+        for unit in end :
+            if len(str(unit)) == 1 :
+                tmp = '0{unit}'.format(unit=unit)
+                init[count] = tmp
+                srtFile.write(f'{tmp}:')
+            elif count == 2 :
+                tmp = '{unit},000'.format(unit=unit)
+                init[count] = tmp
+                srtFile.write(f'{tmp}')
+            else:
+                init[count]  = str(unit)
+                srtFile.write(f' {str(unit)}:')
+            count += 1
+        returnValue = init
+        srtFile.write(f' \n')
+        srtFile.write(f'{data[keys][outputLanguage]}\n')
+        print(f'{returnValue}')     
+    return returnValue
 
 def parseARgs(parser = None ):
     parser.add_argument("-v", "--verbose", action="store_true",
@@ -25,7 +59,7 @@ def parseARgs(parser = None ):
     parser.add_argument("-u", "--url", action="store", default = 'https://youtu.be/aN0-DgKsBxY',
                     help="url de la video téléchargé")
     parser.add_argument("-a", "--audioDir", action="store", default = 'Audio',
-                    help="url de la video téléchargé")
+                    help="repertoire Audio Cache")
     args = parser.parse_args()
     
     if args.verbose:
@@ -51,31 +85,46 @@ def transcribe(chunk :str = '', inputLanguage :str = 'fr', outputLanguage :str =
     translateSubtile = unescape(intputText[0]['translation_text'])
 
     if SousTitre not in exclude :
-        print("#############################")
-        print(f"# Phrases en {inputLanguage} utilisé")
-        print("#############################")
+        print("#############################################")
+        print(f"# Code Language en entrée : {inputLanguage} ")
+        print("#############################################")
         print(SousTitre)
-        print("#############################")
+        print("########################################################################")
         print(f"# Traduction selon le model opus mt {inputLanguage} => {outputLanguage}")
-        print("#############################")
+        print("########################################################################")
         print(translateSubtile)
-        print("#############################")
+        print(f"\n")
+        subtitles = {chunk:{inputLanguage:SousTitre,outputLanguage:translateSubtile}}
+    else :
+        print("#############################################")
+        print(f"# Code Language en entrée : {inputLanguage} ")
+        print("#############################################")
+        print(SousTitre)
+        print("########################################################################")
+        print(f"# Traduction selon le model opus mt {inputLanguage} => {outputLanguage}")
+        print("########################################################################")
+        print('oups what the fluck robot do not understand')
+        print(f"\n")
+        subtitles = {chunk:{inputLanguage:SousTitre,outputLanguage:'oups what the fluck robot do not understand'}}
+    return subtitles
 
 
 def download_video_yt(url: str, audioDir: str ):
     """Download the video url on youtube"""
     file_ = open(file=audioDir+'/buffer-rt-translate', mode='wb+')
     try:
-        yt = YouTube(url)
+        yt = YouTube(url, use_oauth=False, allow_oauth_cache=True)
     except:
         traceback.print_exc()
+    if yt.age_restricted:
+        yt.bypass_age_gate()
     
     for key, values in yt.streams.itag_index.items() :
         if values.is_progressive is False and values.audio_codec == 'opus' :
             yt.streams.get_by_itag(key).download(output_path=audioDir,filename='audio.mp3')
     return audioDir+'/audio.mp3'
 
-def split_audio_file(audio_file: str) :    
+def split_audio_file(audio_file: str, audioDir:str) :    
     myaudio = AudioSegment.from_file(audio_file , codec="opus") 
     chunk_length_ms = 10000 # pydub calculates in millisec
     chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of one sec
@@ -84,7 +133,7 @@ def split_audio_file(audio_file: str) :
     splitWav = []
 
     for i, chunk in enumerate(chunks):
-        chunk_name = f"/tmp/chunk{i}.mp3"
+        chunk_name = f"{audioDir}/chunk{i}.mp3"
         chunk.export(chunk_name, format="mp3")
         splitWav.append(chunk_name)
     return splitWav
@@ -110,8 +159,15 @@ openai.api_key = os.environ["OPENAI_KEY"]
 options = parseARgs(argparse.ArgumentParser())
 if __name__ == '__main__':
     initHugeModel()
-    audio_file = download_video_yt(url=options.url,audioDir=options.audioDir)
-    chunks = split_audio_file(audio_file)
-
-    for chunk in chunks :
-        transcribe(chunk = chunk, inputLanguage = options.inputLanguage, outputLanguage = options.outputLanguage)
+    audio_file = download_video_yt(url = options.url,audioDir=options.audioDir)
+    chunks = split_audio_file(audio_file, audioDir = options.audioDir)
+    turn = 0
+    file = open(f"{options.audioDir}/audio.srt", "w+")
+    for chunk in chunks : 
+        srt = transcribe(chunk = chunk, inputLanguage = options.inputLanguage, outputLanguage = options.outputLanguage)
+        if turn == 0 :
+            timescale = generateSrt(srtFile = file, data = srt, outputLanguage = options.outputLanguage)
+        else :
+            timescale = generateSrt(srtFile = file, data = srt, outputLanguage = options.outputLanguage,init = timescale)            
+        turn += 1
+    file.close()
